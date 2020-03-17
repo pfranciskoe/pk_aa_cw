24ef8320b8bc92af29974296dca90fe7a5043e48
peter <pfranciskoe@gmail.com>
2020-03-17 16:58:44 -0700

    added practice work

---
+++ w6d1/rails1-prep-master 2/practice_1/assoc-skeleton/app/models/watch_list_item.rb
@@ -0,0 +1,10 @@
+class WatchListItem < ApplicationRecord
+    belongs_to :watch_list,
+    primary_key: :id,
+    foreign_key: :watch_list_id,
+    class_name: :WatchList
+    belongs_to :company,
+    primary_key: :id,
+    foreign_key: :company_id,
+    class_name: :Company
+end
