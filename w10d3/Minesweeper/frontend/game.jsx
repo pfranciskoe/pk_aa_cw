d9109d34dfb1d210d374ce99519b82b2b4377a17
peter <pfranciskoe@gmail.com>
2020-04-15 16:59:49 -0700

    fully functional minesweaper

--- w10d3/Minesweeper/frontend/game.jsx
+++ w10d3/Minesweeper/frontend/game.jsx
@@ -6,7 +6,7 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      board: new MineSweeper.Board(10, 20)
+      board: new MineSweeper.Board(10, 0)
     }
     this.updateGame = this.updateGame.bind(this)
   }
@@ -21,7 +21,7 @@ class Game extends React.Component {
       <div className="game">
         <h1 className="title">MineSweeper</h1>
         <Board updateGame={this.updateGame} board={this.state.board} />
-        <p className="message"> {this.state.board.lost() ? "oh no!" : null}{this.state.board.won() ? "congrats" : null} </p>
+        <div className="message"> {this.state.board.lost() ? "oh no!" : null}{this.state.board.won() ? "congrats" : null} </div>
       </div>
         
     )
