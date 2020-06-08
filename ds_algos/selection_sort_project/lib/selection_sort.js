b0c9aa0ca76d9a30854a07f673fddd4251786a4f
peter <pfranciskoe@gmail.com>
2020-06-08 10:38:45 -0700

    selectionSort

--- ds_algos/selection_sort_project/lib/selection_sort.js
+++ ds_algos/selection_sort_project/lib/selection_sort.js
@@ -1,9 +1,21 @@
 function swap(arr, index1, index2) {
-
+    let temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+    return arr
 }
 
 function selectionSort(arr) {
-
+    for(let i = 0; i < arr.length - 1; i++){
+        let minIdx = i
+        for(j = i; j < arr.length; j++){
+            if (arr[j] < arr[minIdx]){
+                minIdx = j
+            }
+        }
+        swap(arr,i,minIdx)
+    }
+    return arr
 }
 
 module.exports = {
