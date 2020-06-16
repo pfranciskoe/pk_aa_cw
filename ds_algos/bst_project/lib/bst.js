301e62f368b7f2d7435fe9a49ad6b290842aacf3
peter <pfranciskoe@gmail.com>
2020-06-16 12:24:05 -0700

    bsearch tree

--- ds_algos/bst_project/lib/bst.js
+++ ds_algos/bst_project/lib/bst.js
@@ -8,7 +8,29 @@ class TreeNode {
 
 
 class BST {
-   
+    constructor(){
+       this.root = null
+    }
+    insert(val,root=this.root){
+        if (!this.root){
+           this.root = new TreeNode(val)
+           return;
+        }
+        if (val < root.val){
+            if (this.left){
+                insert(val, this.left)
+            } else{
+                root.left = new TreeNode(val)
+            }
+        } else {
+            if (root.right) {
+                insert(val, this.right)
+            } else {
+                root.right = new TreeNode(val)
+            }
+        }
+
+    }
 }
 
 module.exports = {
