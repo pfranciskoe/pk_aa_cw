119d932d987c3c5cfc7de69b234371f6f08ab970
peter <pfranciskoe@gmail.com>
2020-08-12 15:32:11 -0700

    simplified terminality

--- ds_algos/trie_project/lib/trie.js
+++ ds_algos/trie_project/lib/trie.js
@@ -10,7 +10,26 @@ class Trie {
        this.root = new Node();
    }
    insertRecur(word,root=this.root){
-       if this
+        let letter = word[0]
+        if (!root.children[letter]){
+            root.children[letter] = new Node();
+        }
+        if (word.length === 1) {
+            root.children[letter].isTerminal = true;
+        } else {
+            this.insertRecur(word.slice(1), root.children[letter])
+        }
+   }
+   insertIter(word){
+       let currentNode = this.root
+        for(let i = 0; i < word.length; i++){
+            let letter = word[i]
+            if (!currentNode.children[letter]) {
+                currentNode.children[letter] = new Node();
+            }
+            currentNode = currentNode.children[letter]
+        }
+       currentNode.isTerminal = true;
    }
 }
 
