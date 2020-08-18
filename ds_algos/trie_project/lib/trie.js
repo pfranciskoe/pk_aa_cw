2e49dd3eec026a70eaf66453d1baccc773c05362
peter <pfranciskoe@gmail.com>
2020-08-17 20:51:57 -0700

    com

--- ds_algos/trie_project/lib/trie.js
+++ ds_algos/trie_project/lib/trie.js
@@ -31,6 +31,37 @@ class Trie {
         }
        currentNode.isTerminal = true;
    }
+   searchRecur(word,root=this.root){
+        if (word.length === 0){
+            if (root.isTerminal){
+                return true;
+            } else {
+                return false
+            }
+        }
+        let letter = word[0]
+        if (root.children[letter]){
+            return this.searchRecur(word.slice(1), root.children[letter])
+        } else {
+            return false
+        }
+   }
+    searchIter(word){
+        root = this.root
+        for (let i=0; i<word.length; i++){
+            let letter = word[i]
+            if (!root || !root.children[letter]){
+                return false
+            } else {
+                root = root.children[letter]
+            }
+        }
+        if (root.isTerminal){
+            return false
+        } else {
+            return true;
+        }
+    }
 }
 
 module.exports = {
