421dbdda3b6c44ad3a518b3a869f8e7ee0a4bf1c
peter <pfranciskoe@gmail.com>
2020-08-27 16:02:22 -0700

    add sift

--- heaps_project/lib/max_heap.js
+++ heaps_project/lib/max_heap.js
@@ -1,7 +1,35 @@
 class MaxHeap {
-    
+    constructor(){
+        this.array = [null]
+    }
+    getParent(idx){
+        return Math.floor(idx/2)
+    }
+    getLeftChild(idx){
+        return idx * 2
+    }
+    getRightChild(idx){
+        return (idx*2)+1
+    }
+    siftUp(idx){
+        if (idx === 1) return;
+        let parentIdx = this.getParent(idx);
+        if (this.array[parentIdx] < this.array[idx]){
+            [this.array[parentIdx], this.array[idx]] = [this.array[idx], this.array[parentIdx]]
+            this.siftUp(parentIdx);
+        }
+    }
+    insert(val){
+        this.array.push(val);
+        this.siftUp(this.array.length-1)
+    }
+    siftDown(idx){
+        
+    }
 }
 
 module.exports = {
     MaxHeap
 };
+
+// [null,top1,left2,right3,leftleft4,right5,right6]
