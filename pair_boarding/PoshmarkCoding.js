183a7d29322aaab503c53417b31355d2418c4c0b
peter <pfranciskoe@gmail.com>
2020-09-10 12:30:37 -0700

    go

---
+++ pair_boarding/PoshmarkCoding.js
@@ -0,0 +1,11 @@
+given map:
+    [
+        [0,1,0,1,0]
+        [0,1,0,g,0]
+        [0,1,g,1,0]
+        [0,0,0,1,0]
+        [0,1,0,1,0]
+    ]
+check all spaces that the guard cannot see
+
+check each square
