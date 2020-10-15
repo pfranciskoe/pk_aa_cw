3ec745f12f6cb0c8e6e194ee26a4cef0d6c4d7b5
peter <pfranciskoe@gmail.com>
2020-10-15 11:30:50 -0700

    start translation

--- react-prac/frontend/autocomplete.jsx
+++ react-prac/frontend/autocomplete.jsx
@@ -4,13 +4,15 @@ export default class Autocomplete extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            inputVal: ''
+            inputVal : ''
         }
         this.matches = this.matches.bind(this)
+        this.handleInput = this.handleInput.bind(this)
+        this.handleClick = this.handleClick.bind(this)
     }
     matches(){
-        if(inputVal.length === 0) return this.props.names
-        matchingNames = [];
+        if(this.state.inputVal.length === 0) return this.props.names
+        const matchingNames = [];
         this.props.names.forEach(name=>{
             if(name.slice(0,this.state.inputVal.length)===this.state.inputVal){
                 matchingNames.push(name)
@@ -18,13 +20,26 @@ export default class Autocomplete extends React.Component{
         })
         return matchingNames;
     }
+    handleInput(event){
+        this.setState({inputVal: event.currentTarget.value})
+    }
+    handleClick(event){
+        this.setState({ inputVal: event.currentTarget.innerText })
+    }
     render(){
+        const matches = this.matches()
         return(
             <div className='auto-complete'>
-                <input type="text" className='auto-complete__input'/>
+                <input
+                    className='auto-complete__input' 
+                    onChange={this.handleInput}
+                    placeholder='Search...'
+                    value={this.state.inputVal}/>   
                 <ul className='auto-complete__list'>
-                    {this.matches.map((name,index)=>(
-                        <li key={index}>name</li>
+                    {matches.map((name,index)=>(
+                        <li key={index} onClick={this.handleClick}>
+                            {name}
+                        </li>
                     ))}
                 </ul>
             </div>
